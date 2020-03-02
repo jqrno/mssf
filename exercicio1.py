f19823688b1b18c90fc33b4a126172d51372917a
jqrno <54086904+jqrno@users.noreply.github.com>
2020-03-02 16:05:05 -0300

    Letra B
    Realização da letra b. Melhorias diversas. Uso de bibliotecas: matplotlib, pandas e sympy.

--- exercicio1.py
+++ exercicio1.py
@@ -1,9 +1,24 @@
-def funcao(t):
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sym
+import numpy as np
+
+
+def x(t):
     return (2*(t**3)) - (10*(t**2)) - (30*t) + 50
 
-print('-----------------')
-print('|   t   |  x(t) |')
-print('-----------------')
-for t in range(11):
-    print('|   {}   |   {}   |'.format(t, funcao(t)))
-    print('-----------------')
+def letra_a():
+    xt = []
+    for t in range (11):
+        xt.append(x(t))
+    s = pd.Series(xt)
+    print(s)
+
+def letra_b():
+    t = np.linspace(0, 10, 100)
+    plt.plot(t, x(t), label='linear')
+    plt.xlabel('Tempo')
+    plt.ylabel('Posição')
+    plt.title("Posição em função do tempo")
+    plt.legend()
+    plt.show()
